2e8bd7abcbf70fbb4a4efeddbb2b8f1f9ff38e61
AbrahamOdoi <AbrahamOdoi@users.noreply.github.com>
2013-11-14 17:55:35 +0000

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,10 @@
 <?php
 	// mysql_connect('localhost','root','');
 	// mysql_select_db('dbgraphic');
-	mysql_connect('http://216.224.161.207','heart','fs40lP.:W.hA5h7I');
+	$con=mysql_connect('http://216.224.161.207','heart','fs40lP.:W.hA5h7I');
 	mysql_select_db('db_heart_foundation');
+	if(!$con){
+		echo "no connection";
+	}
 ?>
+
